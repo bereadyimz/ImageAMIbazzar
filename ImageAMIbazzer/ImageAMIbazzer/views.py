a8a6a27049a581e2c0f8a395754981ff2a84547e
Md Imtiyaz Alam <84371881+bereadyimz@users.noreply.github.com>
2024-04-28 21:23:19 +0530

    Add files via upload
    Its projects like sandeep Maheswari Imagesbazzer projects Its ecom. projects. Using Html,css,Django python. mainly focuss on Django.

---
+++ ImageAMIbazzer/ImageAMIbazzer/views.py
@@ -0,0 +1,43 @@
+from  django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import redirect
+from myapp.models import *  
+
+
+def show_about_page(request):
+    print("about page request") 
+    name ='learncodewith Durgesh'
+    link ='https://www.youtube.com/learncodewithdurgesh'
+
+
+ 
+    data = {
+         'name': name,
+         'link': link
+    }
+
+
+
+    return  render(request, "about.html", data)  
+
+ 
+def show_home_page(request):
+    cats = Category.objects.all()
+    images = Image.objects.all()
+    data = {'images':images, 'cats':cats}
+    return render(request, "home.html", data)
+
+    
+
+    
+def show_category_page(request, cid):
+    print(cid)
+    cats = Category.objects.all()
+    category = Category.objects.get(pk=cid)
+    images = Image.objects.filter(cat=category) 
+    data = {'images':images, 'cats':cats}
+    return render(request, "home.html", data)
+
+ 
+def home(request):  
+    return redirect('/home')
